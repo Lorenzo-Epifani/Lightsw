569464f9f6c6035172b67a138588b06ac162bc28
Lorenzo Epifani <gbsnlp.94@gmail.com>
2020-04-11 11:08:42 +0200

    added route for event_0

--- Back/lights/routes/index.js
+++ Back/lights/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-const app = express(); 
+const app = express();
+var fs = require('fs');
 router.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   next();
@@ -15,6 +16,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/light', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../data/Light.json'));
+});
+
+router.post('/light/event_0', function(req, res, next) {
+    var fileName = path.join(__dirname, '../data/Light.json');
+    const file = require(fileName);
+    file.light[0] += 1;
+    file.light[1] = !file.light[1];
+
+
+    fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+      if (err) return console.log(err);
+      console.log(JSON.stringify(file));
+      console.log('writing to ' + fileName);
+    });
+    res.sendStatus(200)
+
 });
 
 module.exports = router;
